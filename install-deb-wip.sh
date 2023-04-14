7420706e1496cebc01395a28c8e20b3314bba684
ATAK HQ <113393493+atakhq@users.noreply.github.com>
2023-04-14 10:00:34 -0400

    Update install-deb-wip.sh

--- install-deb-wip.sh
+++ install-deb-wip.sh
@@ -269,7 +269,7 @@ sed -i "s|$search|$replace|" $filename
 
 
 
-read -p "Do you want to setup a FQDN? (y/n) " response
+read -p "Do you want to setup a FQDN? \(y\/n\) " response
 
 if [[ $response =~ ^[Yy]$ ]]; then
 echo "TAK Server SSL Certbot Helper Script"
@@ -288,10 +288,10 @@ echo "You are about to start the letsencrypt cert generation process. "
 echo "When you are ready press any key to resume and follow instructions on screen to create your keys."
 read -p "Press any key to resume setup..."
 
-echo "What is your domain name? (ex: atakhq.com | tak-public.atakhq.com )"
+echo "What is your domain name? \(ex: atakhq.com | tak-public.atakhq.com \)"
 read FQDN
 echo ""
-echo "What is your hostname? (ex: atakhq-com | tak-public-atakhq-com )"
+echo "What is your hostname? \(ex: atakhq-com | tak-public-atakhq-com \)"
 echo "** Suggest using same value you entered for domain name but replace . with -"
 read HOSTNAME
 
